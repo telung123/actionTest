36e6f3237d374dd751b4db5be432212737f94f83
Gabin Kim <mabin0910@gmail.com>
2021-07-27 15:31:02 +0900

    레포/소유자변경

--- action02.js
+++ action02.js
@@ -10,8 +10,8 @@ const octokit = new Octokit({
 
 
 octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
-    owner: 'EntTangunGHEC',
-    repo: 'KakaoSeminar',
+    owner: 'telung123',
+    repo: 'actionTest',
     issue_number: core.getInput('issue_number'),
     labels: [
       'bug'
